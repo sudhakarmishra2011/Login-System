f25942ed4f2a54862f64ce1bdb02926508fa1ba8
Amit <amit20k2014@gmail.com>
2020-12-26 01:04:49 +0530

    Initialized

---
+++ src/Login.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Login() {    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String n=request.getParameter("userName");
+		String p=request.getParameter("userPass");
+		
+		 if(p.equals("servlet")){  
+		        RequestDispatcher rd=request.getRequestDispatcher("servlet2");  
+		        rd.forward(request, response);  
+		    }  
+		    else{  
+		        out.print("Sorry UserName or Password Error!");  
+		        RequestDispatcher rd=request.getRequestDispatcher("/index.html");  
+		        rd.include(request, response);  
+		                      
+		        }  
+		    }  
+		  
+		}  
